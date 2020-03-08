106667dadae44ae524aa3fffb3cafe6fa2086694
Sergey Breslavskiy <bess.nebel@gmail.com>
2020-03-08 16:18:19 +0200

    Unnecessary usings removed

--- Assets/Scripts/LevelSlotController.cs
+++ Assets/Scripts/LevelSlotController.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class LevelSlotController : MonoBehaviour, IPointerClickHandler
